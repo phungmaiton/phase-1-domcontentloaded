e22ff91edd57cf08726d4b49e103f3a34cb3bd44
Mai <garcia.ton94@gmail.com>
2023-05-04 10:28:36 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const text = document.querySelector('#text');
+
+document.addEventListener("DOMContentLoaded", function () {
+    text.textContent = "This is really cool!";
+})
+
+
+
